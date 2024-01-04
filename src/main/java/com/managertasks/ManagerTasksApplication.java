0565cce903dc7c1d277f5629499927ff7416d999
assiszang <assiszang@gmail.com>
2024-01-03 22:02:10 -0300

    feat: Initial commit

---
+++ src/main/java/com/managertasks/ManagerTasksApplication.java
@@ -0,0 +1,13 @@
+package com.managertasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagerTasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagerTasksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/managertasks/ManagerTasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.managertasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagerTasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
